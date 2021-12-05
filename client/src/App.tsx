ec7039df6db0f8dc69d53413a9487fd58937f590
mihailValkov <mihail_valko@mail.bg>
2021-12-05 20:26:07 +0200

    added Categories component and dummy array for categories

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import { FC, lazy, Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router';
+
 import { authenticateAction } from './+store/auth/auth-actions';
 import { AppRootState } from './+store/store';
+
 import Layout from './components/core/Layout';
 import LoadingSpinner from './components/shared/LoadingSpinner';
+
 const Auth = lazy(() => import('./pages/Auth/Auth'));
+const Categories = lazy(() => import('./pages/Categories'));
 
 const App: FC<{}> = () => {
   const dispatch = useDispatch();
@@ -19,6 +23,7 @@ const App: FC<{}> = () => {
     <Layout>
       <Switch>
         <Suspense fallback={<LoadingSpinner />}>
+        <Route path='/categories' exact component={Categories} />
           <Route path='/auth'>
             <Auth isLogged={isLogged} />
           </Route>
