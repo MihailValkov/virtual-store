8137c8fb4cd39a0720ab5c82ae07634feda913df
mihailValkov <mihail_valko@mail.bg>
2021-12-05 21:41:42 +0200

    added Order component and styles

---
+++ client/src/components/orders/Order.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import styles from './Order.module.css';
+import { Link } from 'react-router-dom';
+
+const Order: FC<{}> = (props) => {
+  return (
+    <li className={styles.order}>
+      <header>
+        <h3>Order Number: 123ff234sdf2342asd12</h3>
+        <Link to='/orders/detail/some-id'>Order Details</Link>
+      </header>
+      <div className={styles['order-content']}>
+        <div className={`${styles['order-info']} ${styles.left}`}>
+          <p>Register Date: 6 may 2021, 11:52</p>
+          <p>Address: Bulgaria, Svilengrad</p>
+          <p>Status: Pending</p>
+        </div>
+        <div className={`${styles['order-info']} ${styles.right}`}>
+          <p> 5 x items</p>
+          <p className={styles['total-price']}>159.99 BGN</p>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default Order;
