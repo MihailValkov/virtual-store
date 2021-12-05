0058ba18887cc31462279e18f2f941e932a7fd3f
mihailValkov <mihail_valko@mail.bg>
2021-12-05 20:06:00 +0200

    added route path for Register page

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
 import { Route, useRouteMatch } from 'react-router-dom';
 import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
 
 const Auth: FC<{ isLogged: boolean }> = ({ isLogged }) => {
   const { path } = useRouteMatch();
 
-  return <>{!isLogged && <Route path={`${path}/login`} component={LoginPage} />}</>;
+  return <>
+  {!isLogged && <Route path={`${path}/login`} component={LoginPage} />}
+  {!isLogged && <Route path={`${path}/register`} component={RegisterPage} />}
+  </>;
 };
 
 export default Auth;
