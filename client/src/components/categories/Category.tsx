bb5f19218354c14b31f99fe6da32c220f1087200
mihailValkov <mihail_valko@mail.bg>
2021-12-05 20:28:14 +0200

    Added Category component and styles

---
+++ client/src/components/categories/Category.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Category.module.css';
+
+const Category: FC<{ category: string; imageUrl: string }> = ({ category, imageUrl }) => {
+  return (
+    <li className={styles['category']}>
+      <h3>{category}</h3>
+      <Link to={`/categories?category=${category.toLocaleLowerCase()}`}>
+        <img src={imageUrl} alt={category} />
+      </Link>
+    </li>
+  );
+};
+
+export default Category;
