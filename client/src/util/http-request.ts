527b32f6e6e5b1b1c5bf3fa55c41e0ec5c96dd14
mihailValkov <mihail_valko@mail.bg>
2021-12-05 19:32:26 +0200

    added http-request helper

---
+++ client/src/util/http-request.ts
@@ -0,0 +1,18 @@
+const baseURL = process.env.REACT_APP_API_URL;
+const httpRequest = (method: string, url: string, body: {} | null = null) => {
+  return fetch(`${baseURL}/${url}`, {
+    method,
+    headers: { 'Content-type': 'application/json' },
+    credentials: 'include',
+    body: body ? JSON.stringify(body) : null,
+  })
+    .then((res) => Promise.all([res.json(), res.ok]))
+    .then(([res, isOk]) =>!isOk? (function () {throw new Error(res.message);})(): res);
+};
+
+export const http = {
+  get: httpRequest.bind(null, 'GET'),
+  post: httpRequest.bind(null, 'POST'),
+  put: httpRequest.bind(null, 'PUT'),
+  del: httpRequest.bind(null, 'DELETE'),
+};
